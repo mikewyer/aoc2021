abaa396928d148f4fb228426f578981de02f3e09
Mike Wyer <mike.wyer@gousto.co.uk>
2021-12-10 13:26:09 +0000

    Lint fix.

--- 2021/stub.py
+++ 2021/stub.py
@@ -11,14 +11,7 @@ def example() -> List[str]:
     """.splitlines()
 
 
-def get_inputs() -> List[str]:
-    base, _ = os.path.splitext(os.path.basename(__file__))
-    if os.path.exists(base + ".data"):
-        return [i.strip() for i in open(base + ".data")]
-    return []
-
-
-def part1(inputs) -> int:
+def part1(inputs: List[str]) -> int:  # pylint: disable=unused-argument
     return 0
 
 
@@ -27,13 +20,20 @@ def test_part1() -> None:
     assert part1(inputs) == 1
 
 
-def part2(inputs) -> int:
+def part2(inputs: List[str]) -> int:  # pylint: disable=unused-argument
     return 0
 
 
 def test_part2() -> None:
     inputs = get_inputs()
     assert part2(inputs) == 1
+
+
+def get_inputs() -> List[str]:
+    base, _ = os.path.splitext(os.path.basename(__file__))
+    if os.path.exists(base + ".data"):
+        return [i.strip() for i in open(base + ".data")]
+    return []
 
 
 def test_get_inputs() -> None:
